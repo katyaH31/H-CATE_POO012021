7c5a426c7ab0a580428756acfae4275c03f54968
Agustinbarrios017 <00188119@uca.edu.sv>
2021-03-12 23:42:59 -0600

    initialized project

---
+++ TeamWork/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TeamWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+
+            Console.WriteLine("Write the first integer value");
+            a = Int32.Parse(Console.ReadLine());
+
+             Console.WriteLine("Write the second integer value");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Adding {a} + {b} equals {AddNumbers(a, b)}");
+
+        }    static int AddNumbers( int a, int b)
+        {
+            return a + b;
+        }
+    }  
+}
+
